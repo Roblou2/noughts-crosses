837169ca04cfafdf07f513463626ccf14a552f8b
Rob Hamburg <robhamburg89@gmail.com>
2023-02-03 20:05:06 +0000

    build logic for game end and stop events

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ for (let z = 0; z < gridSize; z++) {
 })()
 
 //array for keeping track of game
+
 let gameBoard = new Array(9); //records moves on the gameboard.
 
 const Player = (name, sign) => {
@@ -30,13 +31,16 @@ const Player = (name, sign) => {
   const playerOne = Player('player one', 'X')
 
   const playerTwo = Player('player two', '0')
-
+  let end = false;
 
   //scan the array for the sign of the last move and define next array index as sign different to the previous. Set DOM display to this
 function gameTracker () {
 
 
     for (let i = 0; i <gameBoard.length; i++) {
+if (end === true) {
+  return false
+}
 
 if (gameBoard[0] === undefined && i === 0) {
 gameBoard[0] = (playerOne.getSign())
@@ -76,6 +80,8 @@ function reset () {
   playerOneWin.setAttribute('style', 'display: none;')
   playerTwoWin.setAttribute('style', 'display: none;')
   gameBoard = new Array(9)
+  end = false
+        
  }
 }
 
@@ -86,76 +92,111 @@ let square = document.querySelectorAll('.square')
 
 const int = (() => {
 for (let z = 0; z < square.length; z++) {
-square[z].addEventListener('click', gameTracker)
+  square[z].addEventListener('click', gameTracker)
 square[z].addEventListener('click', () => {
-
+ 
     //PLAYER 1 WIN TRACKER 
     //horizontal wins for player 1
     if (square[0].textContent == 'X' && square[1].textContent == 'X' && square[2].textContent == 'X') {
-        return playerOneWin.setAttribute('style', 'display: block')
+   playerOneWin.setAttribute('style', 'display: block')
+ end = true
+ return false
+
+   
         }
         else if (square[3].textContent == 'X' && square[4].textContent == 'X' && square[5].textContent == 'X') {
-         return playerOneWin.setAttribute('style', 'display: block')
+        playerOneWin.setAttribute('style', 'display: block')
+        end = true
+        return false
             }
             else if (square[6].textContent == 'X' && square[7].textContent == 'X' && square[8].textContent == 'X') {
-               return playerOneWin.setAttribute('style', 'display: block')
+            playerOneWin.setAttribute('style', 'display: block')
+            end = true
+        return false
                 }
                 //vertical wins for player 1
 
                 if (square[0].textContent == 'X' && square[3].textContent == 'X' && square[6].textContent == 'X') {
-                  return playerOneWin.setAttribute('style', 'display: block')
+                   playerOneWin.setAttribute('style', 'display: block')
+                   end = true
+        return false
                     }
                     else if (square[1].textContent == 'X' && square[4].textContent == 'X' && square[7].textContent == 'X') {
-                     return playerOneWin.setAttribute('style', 'display: block')
+                 playerOneWin.setAttribute('style', 'display: block')
+                 end = true
+        return false
                         }
                         else if (square[2].textContent == 'X' && square[5].textContent == 'X' && square[8].textContent == 'X') {
-                           return playerOneWin.setAttribute('style', 'display: block')
+                        playerOneWin.setAttribute('style', 'display: block')
+                        end = true
+        return false
                             }
 
                             // diagonal wins player 1
                             else if (square[0].textContent == 'X' && square[4].textContent == 'X' && square[8].textContent == 'X') {
-                              return playerOneWin.setAttribute('style', 'display: block')
+                              playerOneWin.setAttribute('style', 'display: block')
+                              end = true
+        return false
                                    }
                                    else if (square[2].textContent == 'X' && square[4].textContent == 'X' && square[6].textContent == 'X') {
-                                    return playerOneWin.setAttribute('style', 'display: block')
+                                   playerOneWin.setAttribute('style', 'display: block')
+                                   end = true
+        return false
                                        }
 
                                        //PLAYER 2 WIN TRACKER
 
                                        //horizontal wins player 2
                                      if (square[0].textContent == '0' && square[1].textContent == '0' && square[2].textContent == '0') {
-                                       return playerTwoWin.setAttribute('style', 'display: block')
+                                  playerTwoWin.setAttribute('style', 'display: block')
+                                  end = true
+        return false
+                                    
                                         }
                                         else if (square[3].textContent == '0' && square[4].textContent == '0' && square[5].textContent == '0') {
-                                          return playerTwoWin.setAttribute('style', 'display: block')
+                                          playerTwoWin.setAttribute('style', 'display: block')
+                                          end = true
+        return false
                                             }
                                             else if (square[6].textContent == '0' && square[7].textContent == '0' && square[8].textContent == '0') {
-                                             return playerTwoWin.setAttribute('style', 'display: block')
+                                             playerTwoWin.setAttribute('style', 'display: block')
+                                             end = true
+        return false
                                                 }
 
                                                     //vertical wins for player 2
 
                 if (square[0].textContent == '0' && square[3].textContent == '0' && square[6].textContent == '0') {
-                  return playerTwoWin.setAttribute('style', 'display: block')
+                  playerTwoWin.setAttribute('style', 'display: block')
+                  end = true
+        return false
                     }
                     else if (square[1].textContent == '0' && square[4].textContent == '0' && square[7].textContent == '0') {
-                     return playerTwoWin.setAttribute('style', 'display: block')
+                    playerTwoWin.setAttribute('style', 'display: block')
+                    end = true
+        return false
                         }
                         else if (square[2].textContent == '0' && square[5].textContent == '0' && square[8].textContent == '0') {
-                           return playerTwoWin.setAttribute('style', 'display: block')
+                           playerTwoWin.setAttribute('style', 'display: block')
+                           end = true
+        return false
                             }
 
                                   // diagonal wins player 2
                                   else if (square[0].textContent == '0' && square[4].textContent == '0' && square[8].textContent == '0') {
-                                    return playerTwoWin.setAttribute('style', 'display: block')
+                                 playerTwoWin.setAttribute('style', 'display: block')
+                                 end = true
+        return false
                                        }
                                        else if (square[2].textContent == '0' && square[4].textContent == '0' && square[6].textContent == '0') {
-                                          return playerTwoWin.setAttribute('style', 'display: block')
+                                          playerTwoWin.setAttribute('style', 'display: block')
+                                          end = true
+        return false
                                            }
-    
+                                         
 
 
-})
+}, true)
 
 }
 })()
