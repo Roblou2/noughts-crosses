d435483c876863d0e260ce3c8c0d866eed9ec5ed
Rob Hamburg <robhamburg89@gmail.com>
2023-02-05 16:53:08 +0000

    finish for desktop

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ let name2 = window.prompt("Enter name for player 2 (0)")
 //display for player names
 let first = document.querySelector('.players p.first')
 let second = document.querySelector('.players p.second')
-first.textContent = name1
-second.textContent = name2
+first.textContent = name1 + '(X)'
+second.textContent = name2 + '(0)'
 
 //array for keeping track of game
 
@@ -47,7 +47,7 @@ const Player = (name, sign) => {
 
   //scan the array for the sign of the last move and define next array index as sign different to the previous. Set DOM display to this
 function gameTracker () {
-
+  turnDisplay()
 
     for (let i = 0; i <gameBoard.length; i++) {
 if (end === true) {
@@ -56,20 +56,23 @@ if (end === true) {
 
 if (gameBoard[0] === undefined && i === 0) {
 gameBoard[0] = (playerOne.getSign())
+
 return this.textContent = gameBoard[0]
                               //get sign for playerOne upon start upon and push too gameBoard[0]
     }
 
 else if (gameBoard[i] === undefined && gameBoard[i-1] === 'X') {
-  
+
 gameBoard[i] = (playerTwo.getSign())  
+turnDisplay()
  //get sign for playerTwo ('0') and push
 return this.textContent = gameBoard[i]
 }
 
 else if (gameBoard[i] === undefined && gameBoard[i-1] === '0') {
-gameBoard[i] = (playerOne.getSign())
 
+gameBoard[i] = (playerOne.getSign())
+turnDisplay()
 return this.textContent = gameBoard[i]
 }
 else if (this.textContent == 'X' || this.textContent == '0') { //logic for squares already filled
@@ -93,6 +96,13 @@ let playerTWoWinScore = document.querySelector('.player2 p.second-score')
 playerOneWinScore.textContent = name1
 playerTWoWinScore.textContent = name2
 
+let turnOne = document.querySelector('.turn p.turn1')
+let turnTwo = document.querySelector('.turn p.turn2')
+
+turnOne.textContent = `It's ${name1}'s turn`
+turnTwo.textContent = `It's ${name2}'s turn`
+
+
 function reset () {
  for (let i = 0; i < square.length; i++) {
   square[i].textContent = ""
@@ -100,6 +110,8 @@ function reset () {
   playerOneWin.setAttribute('style', 'display: none;')
   playerTwoWin.setAttribute('style', 'display: none;')
   draw.setAttribute('style', 'display: none')
+  turnTwo.setAttribute('style', 'display: none')
+  turnOne.setAttribute('style', 'display: block')
   gameBoard = new Array(9)
   end = false
         
@@ -113,6 +125,8 @@ let square = document.querySelectorAll('.square')
 
 const int = (() => {
 for (let z = 0; z < square.length; z++) {
+
+
   square[z].addEventListener('click', gameTracker)
 square[z].addEventListener('click', () => {
  
@@ -285,3 +299,27 @@ return false
 
 }
 })()
+
+function turnDisplay () { 
+  
+
+  for (let x = 0; x < gameBoard.length; x++) {
+   
+if (gameBoard[0] == undefined) {  //upon game start
+  turnTwo.setAttribute('style', 'display: block')
+return   turnOne.setAttribute('style', 'display: none')
+}
+
+  else if (gameBoard[x-1] == 'X' && gameBoard[x] == undefined) {
+      turnOne.setAttribute('style', 'display: none')
+ return  turnTwo.setAttribute('style', 'display: block')
+    }
+    
+    else if (gameBoard[x-1] == '0' && gameBoard[x] == undefined) {
+      turnTwo.setAttribute('style', 'display: none')
+ return   turnOne.setAttribute('style', 'display: block')
+  
+    }
+  
+ }
+}
